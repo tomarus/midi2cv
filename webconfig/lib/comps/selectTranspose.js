1aae22ce5b0ec77856199510903ee8db474e5cb3
Tommy van Leeuwen <tommy@debian.chiparus.net>
2019-07-07 22:43:50 +0200

    Enlarge transpose range.

--- webconfig/lib/comps/selectTranspose.js
+++ webconfig/lib/comps/selectTranspose.js
@@ -5,9 +5,9 @@ export default ((props) =>
 	<FormGroup>
 		<FormControl componentClass='select' value={props.transpose}
 			onChange={(e) => { props.onChange(props.index, parseInt(e.target.value, 10)) }}>
-			<option key={0} value={0}>Unknown</option>
-			{[...Array(97).keys()].map((index) =>
-				<option key={index - 48 + 64} value={index - 48 + 64}>{index - 48}</option>
+			<option key={-1} value={-1}>Unknown</option>
+			{[...Array(127).keys()].map((index) =>
+				<option key={index+1} value={index+1}>{index - 63}</option>
 			)}
 		</FormControl>
 	</FormGroup>
